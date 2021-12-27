724ca07c59777a730403e9479091b5c078f0dadb
Oleh Vdovychenko <ovdovychenko@corelogic.com>
2021-12-27 13:43:03 +0200

    Added front-end part with Angular

--- src/main/java/com/vdovychenko/onlineshop/repository/ProductRepository.java
+++ src/main/java/com/vdovychenko/onlineshop/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.vdovychenko.onlineshop.repository;
 import com.vdovychenko.onlineshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
