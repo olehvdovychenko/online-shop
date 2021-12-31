669ebb8b6827b598b4f9a8a4203a42359c227020
Oleh Vdovychenko <ovdovychenko@corelogic.com>
2021-12-31 16:23:06 +0200

    Refactored product-category menu

--- front-end/online-shop-ui/src/app/services/product.service.ts
+++ front-end/online-shop-ui/src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
 import {Product} from "../classes/product";
+import {ProductCategory} from "../classes/product-category";
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,8 @@ export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products'
 
+  private categoryUrl = 'http://localhost:8080/api/product-category'
+
   constructor(private httpClient: HttpClient) {
   }
 
@@ -18,14 +21,26 @@ export class ProductService {
 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
+
+  getProductCategory(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    )
+  }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
--- src/main/java/com/vdovychenko/onlineshop/config/DataRestConfig.java
+++ src/main/java/com/vdovychenko/onlineshop/config/DataRestConfig.java
@@ -1,15 +1,29 @@
-package com.vdovychenko.onlineshop.repository;
+package com.vdovychenko.onlineshop.config;
 
 import com.vdovychenko.onlineshop.entity.Product;
 import com.vdovychenko.onlineshop.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public DataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -24,5 +38,20 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config){
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for(EntityType temp: entities){
+            entityClasses.add(temp.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
